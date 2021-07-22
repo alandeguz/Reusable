ab5d45ae539028d1b293da3c50b54fa5c116c7ed
Alan DeGuzman <alan.a.deguzman@ehi.com>
2021-07-22 09:59:46 -0500

    Package update to change dependency to iOS 11

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Reusable",
-    platforms: [.iOS(.v8), .tvOS(.v9)],
+    platforms: [.iOS(.v11), .tvOS(.v9)],
     products: [
         .library(name: "Reusable", targets: ["Reusable"])
     ],
